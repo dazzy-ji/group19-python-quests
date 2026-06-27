922b0f52637a829725668479817a4c47e86e9515
dazzy-ji <d.indimuli@alustudent.com>
2026-06-27 16:55:49 +0200

    Quest 26 completed

---
+++ quests/quest_26_simple_calculator.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def modulus(a, b):
+    return(a % b)
+
+firstNumber = float(input("Enter the first number: "))
+secondNumber = float(input("Enter the second number: "))
+operation = input("Operation: (add, subtract, multiply, divide, modulus): ").lower()
+
+if operation == "add":
+    result = firstNumber + secondNumber
+elif operation == "subtract":
+    result = firstNumber - secondNumber
+elif operation == "multiply":
+    result = firstNumber * secondNumber
+elif operation == "divide":
+    result = firstNumber / secondNumber
+elif operation == "modulus":
+    result = firstNumber % secondNumber
+else:
+    print("Invalid option")
+
+print("Result: ", result)
