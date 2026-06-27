ddeabff1464a14dc8e9af7b9cfc1994e1b56b446
ikariuki-crypto <i.kariuki@alustudent.com>
2026-06-27 22:23:58 +0200

    Quest 1 2 3 4 complete

---
+++ Quests/quest_01_first_spell.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Quest 1: Your first Spell
+
+welcome_message = "Welcome to the grand adventure, fellow traveller! May your code compile on the first try."
+print(welcome_message)
---
+++ Quests/quest_04_the_town_crier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Quest 4: The Town Crier
+
+city_name = "Valoria"
+current-Year = 2026
+resident_name = "Eldrin"
+
+print(f"Welcome to {city_name}! the year is {current_year}, and our newest resident is {resident_name.")
