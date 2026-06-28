1d545b4599d21d95b1acf53c8ea01c85220a3380
dazzy-ji <d.indimuli@alustudent.com>
2026-06-28 14:14:05 -0700

    files deleted

--- quest_9.py
+++
@@ -1,6 +0,0 @@
-gold = 27
-friends = 4
-each_friend_gets = gold // friends
-remainder = gold % friends
-print(f"Each friend gets {each_friend_gets} pieces of gold")
-print(f"The goblin remains with {remainder} pieces of gold")
